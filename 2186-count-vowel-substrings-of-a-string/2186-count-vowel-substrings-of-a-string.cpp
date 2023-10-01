dab57b6c28ca403c8ca69a35fe63baf4731f3494
Mitali Potdar <84035896+Mitali-Potdar@users.noreply.github.com>
2023-10-02 01:14:40 +0530

    Time: 3 ms (82.49%), Space: 6.7 MB (64.67%) - LeetHub

--- 2186-count-vowel-substrings-of-a-string/2186-count-vowel-substrings-of-a-string.cpp
+++ 2186-count-vowel-substrings-of-a-string/2186-count-vowel-substrings-of-a-string.cpp
@@ -1,79 +1,59 @@
-// class Solution {
-// public:
-//     bool isVowel(char c) {
-//         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
-//     }
-//     int solve(string str, int start, int end){
-//         unordered_map<char, int> m;
-//         for(int i = start; i <= end; i++){
-//             m[str[i]]++;
-
-//         }
-//     }
-//     int countVowelSubstrings(string str) {
-//         int n = str.size();
-//         int count = 0;
-//         for(int i = 0; i < n; i++){
-//             if(isVowel(str[i])){
-//                 int j = i+1;
-//                 while(isVowel(str[i]) && j < n) j++;
-//                 count += solve(str, i, j-1); //solving for entire block of all vowels
-//                 i = j;
-//             }
-//         }
-//         // for(int i = 0; i < n; i++){
-//         //     unordered_set<char> s;
-            
-//         //     for(int j = i; j < n; j++){
-//         //         if(isVowel(str[j])){
-//         //             s.insert(str[j]);
-//         //         }
-//         //         else
-//         //             break;
-                
-//         //         if(s.size() == 5){
-//         //             count++;
-//         //         }
-//         //     }
-//         // }
-//         return count;
-//     }
-// };
-
-
-
-
 class Solution {
 public:
-    int countVowelSubstrings(string word) {
-        
-        unordered_map<char,int>m;
-        int n=word.size();
-        int ans=0;
-        
-        for(int i=0,cnt=0,j=0,pref=0;i<n;i++){
-            if((word[i]=='a'||word[i]=='e'||word[i]=='i'||word[i]=='o'||word[i]=='u'))
-            {
-                if(m[word[i]]++==0)
-                     cnt++;
-                
-                while(m[word[j]]>1)
-				m[word[j++]]--,pref++;
-                
-            if(cnt==5)ans+=(1+pref);
+    bool isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+    int solve(string str, int start, int end){
+        unordered_map<char, int> m;
+        int cnt = 0;
+        for(int i = start; i <= end; i++){
+            m[str[i]]++;
+            if(m.size() == 5){
+                int k = start;
+                while(m.size() == 5){
+                    m[str[k]]--;
+                    if(m[str[k]] == 0){
+                        m.erase(str[k]);
+                        break;
+                    }
+                    k++;       
+                }
+                int prefix = k - start + 1;
+                int suffix = end - i + 1;
+                cnt += prefix*suffix;
+                start = k+1;
             }
-               else{
-               
-                   cnt=0;
-                   pref=0;
-                   m.clear();
-                   j=i+1;
-               }
-                   
-            
-            
+
         }
-        return ans;
+        return cnt;
+    }
+    int countVowelSubstrings(string str) {
+        int n = str.size();
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            if(isVowel(str[i])){
+                int j = i+1;
+                while(j < n && isVowel(str[j])) j++;
+                count += solve(str, i, j-1); //solving for entire block of all vowels
+                i = j;
+            }
+        }
+        // for(int i = 0; i < n; i++){
+        //     unordered_set<char> s;
+            
+        //     for(int j = i; j < n; j++){
+        //         if(isVowel(str[j])){
+        //             s.insert(str[j]);
+        //         }
+        //         else
+        //             break;
+                
+        //         if(s.size() == 5){
+        //             count++;
+        //         }
+        //     }
+        // }
+        return count;
     }
 };
 

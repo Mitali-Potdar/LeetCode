e75b2213b5f7a5247702c6769bb3c099a8553388
Mitali Potdar <84035896+Mitali-Potdar@users.noreply.github.com>
2023-10-02 00:52:21 +0530

    Time: 3 ms (82.3%), Space: 6.6 MB (70.18%) - LeetHub

--- 2186-count-vowel-substrings-of-a-string/2186-count-vowel-substrings-of-a-string.cpp
+++ 2186-count-vowel-substrings-of-a-string/2186-count-vowel-substrings-of-a-string.cpp
@@ -1,56 +1,79 @@
+// class Solution {
+// public:
+//     bool isVowel(char c) {
+//         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+//     }
+//     int solve(string str, int start, int end){
+//         unordered_map<char, int> m;
+//         for(int i = start; i <= end; i++){
+//             m[str[i]]++;
+
+//         }
+//     }
+//     int countVowelSubstrings(string str) {
+//         int n = str.size();
+//         int count = 0;
+//         for(int i = 0; i < n; i++){
+//             if(isVowel(str[i])){
+//                 int j = i+1;
+//                 while(isVowel(str[i]) && j < n) j++;
+//                 count += solve(str, i, j-1); //solving for entire block of all vowels
+//                 i = j;
+//             }
+//         }
+//         // for(int i = 0; i < n; i++){
+//         //     unordered_set<char> s;
+            
+//         //     for(int j = i; j < n; j++){
+//         //         if(isVowel(str[j])){
+//         //             s.insert(str[j]);
+//         //         }
+//         //         else
+//         //             break;
+                
+//         //         if(s.size() == 5){
+//         //             count++;
+//         //         }
+//         //     }
+//         // }
+//         return count;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
-    bool isVowel(char c) {
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
-    }
-    int atMostK(string str, int k){
-        int n = str.size();
-        int res = 0;
-        unordered_map<char, int> mp;
-        int l = 0;
-        for(int r = 0; r < n; r++){
-            if(!isVowel(str[r])){
-                    mp.clear();
-                    l = r + 1;
-                    continue;
-            }
-            else
+    int countVowelSubstrings(string word) {
+        
+        unordered_map<char,int>m;
+        int n=word.size();
+        int ans=0;
+        
+        for(int i=0,cnt=0,j=0,pref=0;i<n;i++){
+            if((word[i]=='a'||word[i]=='e'||word[i]=='i'||word[i]=='o'||word[i]=='u'))
             {
-                mp[str[r]]++;
-    
-                while(l <= r && mp.size() > k){
-                    mp[str[l]]--;
-                    if(mp[str[l]] == 0){
-                        mp.erase(str[l]);
-                    }
-                    l++;
-                }
-                res += r-l+1;
-            }
-        }
-        return res;
-    }
-    int countVowelSubstrings(string str) {
-        //return atMostK(str, 5) - atMostK(str, 4);
-        int n = str.size();
-        int count = 0;
-        
-        for(int i = 0; i < n; i++){
-            unordered_set<char> s;
-            
-            for(int j = i; j < n; j++){
-                if(isVowel(str[j])){
-                    s.insert(str[j]);
-                }
-                else
-                    break;
+                if(m[word[i]]++==0)
+                     cnt++;
                 
-                if(s.size() == 5){
-                    count++;
-                }
+                while(m[word[j]]>1)
+				m[word[j++]]--,pref++;
+                
+            if(cnt==5)ans+=(1+pref);
             }
+               else{
+               
+                   cnt=0;
+                   pref=0;
+                   m.clear();
+                   j=i+1;
+               }
+                   
+            
+            
         }
-        
-        return count;
+        return ans;
     }
 };
+

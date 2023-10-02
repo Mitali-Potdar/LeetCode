25aa538a0c7c9d757eebd6f12f35ac9de9b34f67
Mitali Potdar <84035896+Mitali-Potdar@users.noreply.github.com>
2023-10-02 19:57:04 +0530

    Time: 29 ms (59.94%), Space: 29 MB (62.34%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int k) {
-        unordered_map<int, int> m;
+    int atMost(vector<int>& nums, int k){
+        if(k < 0)return 0;
+        int l = 0;
         int sum = 0;
-        m[0] = 1;
         int cnt = 0;
-        for(int i = 0; i < nums.size(); i++){
-            sum += nums[i];
-            if(m.find(sum-k) != m.end()){
-                cnt += m[sum-k];
+        for(int r = 0; r < nums.size(); r++){
+            sum += nums[r];
+            while(sum > k){
+                sum -= nums[l];
+                l++;
             }
-            m[sum]++;
+            cnt += r - l + 1;
         }
         return cnt;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int k) {
+        return atMost(nums, k) - atMost(nums, k-1);
+        // unordered_map<int, int> m;
+        // int sum = 0;
+        // m[0] = 1;
+        // int cnt = 0;
+        // for(int i = 0; i < nums.size(); i++){
+        //     sum += nums[i];
+        //     if(m.find(sum-k) != m.end()){
+        //         cnt += m[sum-k];
+        //     }
+        //     m[sum]++;
+        // }
+        // return cnt;
     }
 };

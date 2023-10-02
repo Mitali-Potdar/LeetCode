915f978a452b80fa593df4feebebb9022549345b
Mitali Potdar <84035896+Mitali-Potdar@users.noreply.github.com>
2023-10-02 17:01:53 +0530

    Time: 333 ms (63.02%), Space: 90.1 MB (33.81%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -6,13 +6,12 @@ public:
     int eraseOverlapIntervals(vector<vector<int>>& v) {
         sort(v.begin(), v.end(), cmp);
         int r = v[0][1];
-        int cnt = 1;
+        int cnt = 0;
         for(int i = 1; i < v.size(); i++){
-            if(v[i][0] >= r){
+            if(v[i][0] < r){
                 cnt++;
-                r = v[i][1];
-            } 
+            } else  r = v[i][1];
         }
-        return v.size() - cnt;
+        return cnt;
     }
 };

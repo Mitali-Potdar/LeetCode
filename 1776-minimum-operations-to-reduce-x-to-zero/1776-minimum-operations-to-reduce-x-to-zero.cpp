fc549e9aab17e1a1b42415c411f35958844efe37
Mitali Potdar <84035896+Mitali-Potdar@users.noreply.github.com>
2023-10-12 02:33:51 +0530

    Time: 127 ms (57.23%), Space: 98.9 MB (53.08%) - LeetHub

---
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int l = 0, r = 0;
+        int total = 0;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            total += nums[i];
+        }
+        if(x > total) return -1;
+        int target = total - x;
+        int len = INT_MIN;
+        int sum = 0;
+        while(r < n){
+            sum += nums[r];
+            while(sum > target){
+                sum -= nums[l];
+                l++;
+            }
+            if(sum == target) len = max(len, r-l+1);
+            r++;
+        }
+        if(len == INT_MIN) return -1;
+        return n - len;
+    }
+};

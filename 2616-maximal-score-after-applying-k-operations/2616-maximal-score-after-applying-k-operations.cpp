4b1dbf643abfad73c28b7c89486b8d38d8ca0948
Mitali Potdar <84035896+Mitali-Potdar@users.noreply.github.com>
2023-10-23 01:39:35 +0530

    Time: 213 ms (44.84%), Space: 76.2 MB (33.71%) - LeetHub

--- 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
 long long score = 0;
-priority_queue<pair<int,int>> pq;
+priority_queue<int> pq;
 
     long long maxKelements(vector<int>& nums, int k) {
         int n = nums.size();
         for(int i = 0; i < n; i++){
-            pq.push({nums[i], i});
+            pq.push(nums[i]);
         }
         while(k){
-            int val = pq.top().first;
-            int ind = pq.top().second;
+            int val = pq.top();
             pq.pop();
             score += val;
-            if(nums[ind] % 3 == 0) nums[ind] = nums[ind]/3;
-            else nums[ind] = nums[ind]/3 + 1;
-            pq.push({nums[ind], ind});
+            if(val % 3 == 0) val = val/3;
+            else val = val/3 + 1;
+            pq.push(val);
             k--;
         }
         return score;

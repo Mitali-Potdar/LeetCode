42bc7fa54692ec42e9582e63dd3192db240fa985
Mitali Potdar <84035896+Mitali-Potdar@users.noreply.github.com>
2023-10-22 22:14:15 +0530

    Time: 2 ms (41.81%), Space: 6.3 MB (21.58%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     int minBitFlips(int start, int goal) {
         int cnt = 0;
-        while(start && goal){
+        while(start || goal){
             cnt += (start&1) ^ (goal&1);
             start >>= 1;
             goal >>= 1;
         }
-        while(start){
-            cnt += (start&1);
-            start >>= 1;
-        }
-        while(goal){
-            cnt += (goal&1);
-            goal >>= 1;
-        }
+        // while(start){
+        //     cnt += (start&1);
+        //     start >>= 1;
+        // }
+        // while(goal){
+        //     cnt += (goal&1);
+        //     goal >>= 1;
+        // }
         return cnt;
     }
 };

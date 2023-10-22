ebe8d8667c9c1f1836f832ba1ba06cc3346c59e6
Mitali Potdar <84035896+Mitali-Potdar@users.noreply.github.com>
2023-10-23 00:24:02 +0530

    Time: 12 ms (76.5%), Space: 13.6 MB (24.74%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -1,26 +1,49 @@
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
-        int l = 0;
-        int r = 1;
+        //O(N)
+        int n = nums.size();
+        unordered_map<int, int> m;
         int cnt = 0;
-        sort(nums.begin(), nums.end());
-        while(r < nums.size()){
-            if(nums[r] - nums[l] == k){
+        for(int i = 0; i < n; i++){
+            m[nums[i]]++;
+        }  
+        if(k == 0){
+            for(auto v: m){
+               if(v.second > 1)
                 cnt++;
-                l++;
-                r++;
-                while(r < nums.size() && nums[r] == nums[r-1]){
-                    r++;
+            }
+        } else {
+            for(auto v: m){
+                int s = k + v.first;
+                if(m.find(s) != m.end()){
+                    cnt++;
                 }
             }
-            else if(nums[r] - nums[l] > k){
-                l++;
-                if(l == r) r++;
-            } else{
-                r++;
-            }
         }
-        return cnt;    
+        return cnt;
+        //O(NLOGN)
+
+        // int l = 0;
+        // int r = 1;
+        // int cnt = 0;
+        // sort(nums.begin(), nums.end());
+        // while(r < nums.size()){
+        //     if(nums[r] - nums[l] == k){
+        //         cnt++;
+        //         l++;
+        //         r++;
+        //         while(r < nums.size() && nums[r] == nums[r-1]){
+        //             r++;
+        //         }
+        //     }
+        //     else if(nums[r] - nums[l] > k){
+        //         l++;
+        //         if(l == r) r++;
+        //     } else{
+        //         r++;
+        //     }
+        // }
+        // return cnt;    
     }
 };

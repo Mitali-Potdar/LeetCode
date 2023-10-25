36a805a75da0b4fa75d82235cdd4a6c3b478df7a
Mitali Potdar <84035896+Mitali-Potdar@users.noreply.github.com>
2023-10-26 02:48:21 +0530

    Time: 47 ms (22.42%), Space: 25 MB (7.77%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -6,9 +6,22 @@ public:
     int helper(int n, int k){
         if(n == 1)
             return 0;
-        return (helper(n-1,k) + k) % n;
+        return (helper(n-1, k) + k) % n;
     }
     int findTheWinner(int n, int k) {
-        return helper(n, k) + 1; //siince we are taking zero indexed
+        //return helper(n, k) + 1; //since we are taking zero indexed
+        queue<int> q;
+        for(int i=1; i<=n; i++) q.push(i);
+        
+        while(q.size() > 1) {
+            int i = k;
+            while(i > 1) {
+                auto f = q.front(); q.pop();
+                q.push(f);
+                i--;
+            }
+            q.pop();
+        }
+        return q.front();
     }
 };

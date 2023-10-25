2ff30f5f6bc2b49ababa5023f4f7d2703410da0b
Mitali Potdar <84035896+Mitali-Potdar@users.noreply.github.com>
2023-10-26 02:39:52 +0530

    Time: 0 ms (100%), Space: 6.4 MB (61.75%) - LeetHub

---
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+//https://cs.stackexchange.com/questions/7048/a-recursive-formula-for-generalized-josephus-problem
+//Finally only one zero will be left
+//We want the old position of that 0
+    int helper(int n, int k){
+        if(n == 1)
+            return 0;
+        return (helper(n-1,k) + k) % n;
+    }
+    int findTheWinner(int n, int k) {
+        return helper(n, k) + 1; //siince we are taking zero indexed
+    }
+};

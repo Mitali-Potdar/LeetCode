d2b6f43ba5cc0f0bea8f27b545963033a7209b59
Mitali Potdar <84035896+Mitali-Potdar@users.noreply.github.com>
2023-10-27 20:21:33 +0530

    Time: 12 ms (66.76%), Space: 16.9 MB (60.19%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, 0);
-        dp[0] = 0;
-        dp[0] = nums[0];
         for(int i = 1; i < n; i++){
             nums[i] = max(nums[i] + i, nums[i-1]);
         }

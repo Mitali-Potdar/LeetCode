834c29550d193608691bcd8fdd02bd56b5c02252
Mitali Potdar <84035896+Mitali-Potdar@users.noreply.github.com>
2023-10-27 15:06:20 +0530

    Time: 3 ms (29.52%), Space: 7 MB (16.5%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -4,11 +4,18 @@ public:
         if(s.size() != goal.size()) return false;
         if(s == goal) return true;
         int n = goal.size();
+        s = s+s;
         for(int i = 0; i < n; i++){
-            if(s.substr(0, i+1) == goal.substr(n-i-1, i+1) && s.substr(i+1, n-i-1) == goal.substr(0, n-i-1)){
-                return true;
-            }
+            if(s.substr(i, n) == goal) return true;
         }
         return false;
+        // if(s.size() != goal.size()) return false;
+        // if(s == goal) return true;
+        // int n = goal.size();
+        // for(int i = 0; i < n; i++){
+        //     if(s.substr(0, i+1) == goal.substr(n-i-1, i+1) && s.substr(i+1, n-i-1) == goal.substr(0, n-i-1)
+        //         return true;
+        // }
+        // return false;
     }
 };

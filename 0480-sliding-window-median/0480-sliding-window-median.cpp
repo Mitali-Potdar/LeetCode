bc2d9d42f622f161824fa27ce0d656eaf7086f13
Mitali Potdar <84035896+Mitali-Potdar@users.noreply.github.com>
2023-10-28 20:17:03 +0530

    Time: 102 ms (70.23%), Space: 38.5 MB (47.5%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.cpp
+++ 0480-sliding-window-median/0480-sliding-window-median.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     vector<double> medianSlidingWindow(vector<int>& nums, int k) {
-        vector<double> ans;
-        multiset<double> window(begin(nums), begin(nums) + k);
-        auto it = next(begin(window), (k - 1) / 2);      //take the iterator to mid position
-         
-        for(int i=k; ;++i){
-            const double median = ((k & 1) ? *it : (*it + *next(it)) / 2.);
-            ans.emplace_back(median);
-            
-            if(i == nums.size()) break;
-            
+        multiset<int> window(nums.begin(), nums.begin() + k);
+        auto mid = next(window.begin(), k / 2);
+        vector<double> medians;
+        for (int i=k; ; i++) {
+
+            // Push the current median.
+            medians.push_back((double(*mid) + *prev(mid, 1 - k%2)) / 2);
+
+            // If all done, return.
+            if (i == nums.size())
+                return medians;
+                
+            // Insert nums[i].
             window.insert(nums[i]);
-            
-            if(nums[i] < *it)    // If the inserted number is smaller than mid, obviously the iterator must have moved one position ahead..... which means we should take it one step backwards
-                --it;
-            if(nums[i - k] <= *it)    // If the number inserted k steps before is smaller than or equal to the mid, the iterator must have moved one position backwards, because of which we should move it forward by one.
-                ++it;
-            
-            window.erase(window.lower_bound(nums[i - k]));
+            if (nums[i] < *mid)
+                mid--;
+
+            // Erase nums[i-k].
+            if (nums[i-k] <= *mid)
+                mid++;
+            window.erase(window.lower_bound(nums[i-k]));
         }
-        return ans;
     }
 };

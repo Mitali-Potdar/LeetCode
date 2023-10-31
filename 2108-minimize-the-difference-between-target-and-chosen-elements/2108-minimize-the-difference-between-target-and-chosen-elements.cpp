19193edb4dd1a1f2e7b9ada5096e215cdc65b68f
Mitali Potdar <84035896+Mitali-Potdar@users.noreply.github.com>
2023-10-31 16:05:48 +0530

    Time: 1417 ms (49.41%), Space: 19.7 MB (45.97%) - LeetHub

--- 2108-minimize-the-difference-between-target-and-chosen-elements/2108-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 2108-minimize-the-difference-between-target-and-chosen-elements/2108-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -1,26 +1,31 @@
+
 class Solution {
 public:
+    
     int m, n;
-    int dp[70*70+1][70];
-    int dfs(vector<vector<int>>& mat, int r, int sum, int target)
-    {
-        if(r>=m)
-        return abs(sum-target);
-        if(dp[sum][r]!=-1)
-        return dp[sum][r];
-        int ans=INT_MAX;
-        for(int i=0;i<n;i++)
-        {
-            ans=min(ans, dfs(mat, r+1, sum+mat[r][i], target));
-            if(ans==0)
-            break;
-        }
-        return dp[sum][r]=ans;
-    }
+    
     int minimizeTheDifference(vector<vector<int>>& mat, int target) {
-        memset(dp,-1,sizeof(dp));
-        m=mat.size();
-        n=mat[0].size();
-        return dfs(mat, 0, 0, target);
+        
+        m = mat.size();
+        n = mat[0].size();
+        
+        vector<vector<int>> dp(m+1, vector<int>(target+1, INT_MAX));
+        
+        // dp[i][j] ans for upto ith row and j target val
+        
+        for(int j=0; j<=target; j++) dp[0][j] = j;
+        
+        for(int i=1; i<=m; i++){
+            for(int j=0; j <= target; j++){
+                for(int k=0; k<n; k++){
+                    if(j >= mat[i-1][k]) dp[i][j] = min(dp[i][j], dp[i-1][j - mat[i-1][k]]);
+                    else dp[i][j] = min(dp[i][j], (mat[i-1][k] - j) + dp[i-1][0]);
+                }
+            }
+        }
+        
+        int ans = dp[m][target];
+        
+        return ans;
     }
 };

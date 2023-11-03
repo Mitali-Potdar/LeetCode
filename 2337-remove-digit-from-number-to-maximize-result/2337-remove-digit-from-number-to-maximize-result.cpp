41dba920d3f6cc856421eec20ad3985771427514
Mitali Potdar <84035896+Mitali-Potdar@users.noreply.github.com>
2023-11-03 18:46:45 +0530

    Time: 3 ms (42.53%), Space: 6.5 MB (67.32%) - LeetHub

--- 2337-remove-digit-from-number-to-maximize-result/2337-remove-digit-from-number-to-maximize-result.cpp
+++ 2337-remove-digit-from-number-to-maximize-result/2337-remove-digit-from-number-to-maximize-result.cpp
@@ -8,18 +8,16 @@ public:
                 cnt++;
         }
         string res = "";
-        while(cnt > 0){
-            for(int i = 0; i < n; i++){
-                if(number[i] == digit){
-                    if(cnt == 1){
-                        res = number.substr(0, i) + number.substr(i+1, n-i-1);
-                        return res;
-                    }
-                    if(number[i+1] > number[i]){
-                        res = number.substr(0, i) + number.substr(i+1, n-i-1);
-                        return res;
-                    } else cnt--;
+        for(int i = 0; i < n; i++){
+            if(number[i] == digit && cnt > 0){
+                if(cnt == 1){
+                    res = number.substr(0, i) + number.substr(i+1, n-i-1);
+                    return res;
                 }
+                if(number[i+1] > number[i]){
+                    res = number.substr(0, i) + number.substr(i+1, n-i-1);
+                    return res;
+                } else cnt--;
             }
         }
         return res;
